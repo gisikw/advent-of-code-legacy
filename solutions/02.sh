42ea44471e21d15a62320127c1e99ecdf0c0102f
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-02 07:08:02 -0600

    Day two optimization

--- solutions/02.sh
+++ solutions/02.sh
@@ -20,39 +20,41 @@ convert() {
 if [ $part -eq 1 ]; then
   score=0
   while read pair; do
-    mine=$(echo $pair | cut -d' ' -f2 | convert) 
-    yours=$(echo $pair | cut -d' ' -f1 | convert)
+    count=$(echo $pair | cut -d' ' -f1)
+    yours=$(echo $pair | cut -d' ' -f2 | convert)
+    mine=$(echo $pair | cut -d' ' -f3 | convert) 
 
-    score=$(($score+$mine)) 
+    score=$(($score+($mine*count))) 
     if [ $mine -eq $yours ]; then
-      score=$(($score+3))
+      score=$(($score+3*count))
     else 
       if ([ $mine -eq 1 ] && [ $yours -eq 3 ]) || ([ $mine -eq 2 ] && [ $yours -eq 1 ]) || ([ $mine -eq 3 ] && [ $yours -eq 2 ]); then
-        score=$(($score+6))
+        score=$(($score+6*$count))
       fi
     fi
-  done < <(cat $input)
+  done < <(sort $input | uniq -c)
   echo $score
 else
   score=0
   while read pair; do
-    mine=$(echo $pair | cut -d' ' -f2 | convert) 
-    yours=$(echo $pair | cut -d' ' -f1 | convert)
+    count=$(echo $pair | cut -d' ' -f1)
+    yours=$(echo $pair | cut -d' ' -f2 | convert)
+    mine=$(echo $pair | cut -d' ' -f3 | convert) 
 
     case $mine in
       1)
         loser=$(($yours-1))
         [ $loser -eq 0 ] && loser=3
-        score=$(($score+$loser))
+        score=$(($score+$loser*count))
         ;;
       2)
-        score=$(($score+$yours+3))
+        score=$(($score+($yours+3)*count))
         ;;
       3)
         winner=$(expr $yours % 3 + 1)
-        score=$(($score+$winner+6))
+        score=$(($score+($winner+6)*count))
         ;;
     esac
-  done < <(cat $input)
+  done < <(sort $input | uniq -c)
   echo $score
 fi
