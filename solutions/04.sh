31de9612cf78519165c281e041272082092ce2fe
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-03 23:58:44 -0600

    Perf fix for day 4

--- solutions/04.sh
+++ solutions/04.sh
@@ -5,15 +5,13 @@ part=${2:-1}
 score=0
 if [ $part -eq 1 ]; then
   while read line; do
-    line=$(echo $line | sed 's/[,-]/ /g')
     read low_a high_a low_b high_b <<< $line
     [ $((($low_a-$low_b)*(($high_a-$high_b)))) -le 0 ] && ((score+=1))
-  done < <(cat $input)
+  done < <(cat $input | sed 's/[,-]/ /g')
 else
   while read line; do
-    line=$(echo $line | sed 's/[,-]/ /g')
     read low_a high_a low_b high_b <<< $line
     [ $low_b -le $high_a ] && [ $low_a -le $high_b ] && ((score+=1))
-  done < <(cat $input)
+  done < <(cat $input | sed 's/[,-]/ /g')
 fi
 echo $score
