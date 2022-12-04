5f685b101bb225d8284b27685456c7d429f50098
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-04 08:21:23 -0600

    Update day 04

--- solutions/04.sh
+++ solutions/04.sh
@@ -5,13 +5,13 @@ part=${2:-1}
 score=0
 if [ $part -eq 1 ]; then
   while read line; do
-    read low_a high_a low_b high_b <<< $line
-    [ $((($low_a-$low_b)*(($high_a-$high_b)))) -le 0 ] && ((score+=1))
+    read A B C D <<< $line
+    [ $(((A-C)*(B-D))) -le 0 ] && ((score+=1))
   done < <(cat $input | sed 's/[,-]/ /g')
 else
   while read line; do
-    read low_a high_a low_b high_b <<< $line
-    [ $low_b -le $high_a ] && [ $low_a -le $high_b ] && ((score+=1))
+    read A B C D <<< $line
+    [ $(((A-D)*(B-C))) -le 0 ] && ((score+=1))
   done < <(cat $input | sed 's/[,-]/ /g')
 fi
 echo $score
