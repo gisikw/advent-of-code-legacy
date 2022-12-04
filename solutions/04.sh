5a26738678e958b250034c3e049fabbf4d5bc5c6
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-03 23:55:39 -0600

    Optimizing day four

--- solutions/04.sh
+++ solutions/04.sh
@@ -5,23 +5,15 @@ part=${2:-1}
 score=0
 if [ $part -eq 1 ]; then
   while read line; do
-    line=$(echo $line | sed 's/[^0-9]/ /g')
-    read -r low_a high_a low_b high_b < <(awk '{print $1,$2,$3,$4}' <<< $line)
-    if [ $low_a -ge $low_b ] && [ $high_a -le $high_b ]; then
-      ((score+=1))
-    else
-      if [ $low_a -le $low_b ] && [ $high_a -ge $high_b ]; then
-        ((score+=1))
-      fi
-    fi
+    line=$(echo $line | sed 's/[,-]/ /g')
+    read low_a high_a low_b high_b <<< $line
+    [ $((($low_a-$low_b)*(($high_a-$high_b)))) -le 0 ] && ((score+=1))
   done < <(cat $input)
 else
   while read line; do
-    line=$(echo $line | sed 's/[^0-9]/ /g')
-    read -r low_a high_a low_b high_b < <(awk '{print $1,$2,$3,$4}' <<< $line)
-    if [ $low_b -le $high_a ] && [ $low_a -le $high_b ]; then
-      ((score+=1))
-    fi
+    line=$(echo $line | sed 's/[,-]/ /g')
+    read low_a high_a low_b high_b <<< $line
+    [ $low_b -le $high_a ] && [ $low_a -le $high_b ] && ((score+=1))
   done < <(cat $input)
 fi
 echo $score
