a2f75b642da1a16f19b1405ebfe0b1b10f437689
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-06 08:05:35 -0600

    Day six improvement

--- solutions/06.sh
+++ solutions/06.sh
@@ -3,5 +3,15 @@ input=$1
 part=${2:-1}
 
 [ $part -eq 1 ] && len=4 || len=14; i=0; signal=$(cat $input)
-while grep '\(.\).*\1' >/dev/null <<< ${signal:$i:$len}; do ((i++)); done
+while true; do
+  read dup < <(awk -v FS="" '{
+    for(i=1;i<=NF;i++) {
+      if(a[$i]!=0) break
+      a[$i]=i
+    }
+  }
+  END {print a[$i]}' <<< ${signal:$i:$len})
+  [ -z "$dup" ] && break
+  ((i+=dup))
+done
 echo $((i+len))
