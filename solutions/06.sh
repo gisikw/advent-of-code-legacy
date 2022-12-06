46b83ad4a851e38a96734e97eebfe47103b9c40d
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-06 00:09:32 -0600

    Hopefully last refactor

--- solutions/06.sh
+++ solutions/06.sh
@@ -3,5 +3,5 @@ input=$1
 part=${2:-1}
 
 [ $part -eq 1 ] && len=4 || len=14; i=0; signal=$(cat $input)
-until [ -z $(echo ${signal:$i:$len} | grep '\(.\).*\1') ]; do ((i++)); done
+while grep '\(.\).*\1' >/dev/null <<< ${signal:$i:$len}; do ((i++)); done
 echo $((i+len))
