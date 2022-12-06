571c6b66c0c0f61e7cd348ac3cc6459be0fc501a
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-05 23:52:56 -0600

    More code golf

--- solutions/06.sh
+++ solutions/06.sh
@@ -2,19 +2,6 @@
 input=$1
 part=${2:-1}
 
-if [ $part -eq 1 ]; then
-  len=4
-else
-  len=14
-fi
-
-read signal < <(cat $input)
-idx=$len
-while true; do
-  if [ -z $(echo ${signal:0:$len} | grep '\(.\).*\1') ]; then
-    echo $idx
-    break;
-  fi
-  ((idx++))
-  signal=${signal:1}
-done
+[ $part -eq 1 ] && len=4 || len=14; i=0; signal=$(cat $input)
+until [ -z $(echo ${signal:$i:$len} | grep '\(.\).*\1') ]; do ((i++)); done
+echo $((i+len))
