5bb9f0d91faa0ab7f4d80f8251b61df15b57c1d9
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-07 00:29:33 -0600

    Day Seven cleanup

--- solutions/07.sh
+++ solutions/07.sh
@@ -3,48 +3,39 @@ input=$(realpath $1)
 part=${2:-1}
 old_cwd=$CWD
 
+# What is a filesystem but a tree with prebuilt utilities?
 playground=$(mktemp -d)
 cd $playground
 while read cmd; do
   if [ "${cmd:0:1}" == "$" ]; then
     if [ "${cmd:2:2}" == "cd" ]; then
-      case "${cmd:5}" in
-        /)
-          cd $playground
-          ;;
-        ..)
-          cd ..
-          ;;
-        *)
-          mkdir -p "${cmd:5}" && cd "${cmd:5}"
-          ;;
+      path="${cmd:5}"
+      case $path in
+        /) cd $playground;;
+        ..) cd ..;;
+        *) mkdir -p $path && cd $path;;
       esac
     fi
   else
     read size name <<< $cmd
-    if [ "$size" == "dir" ]; then
-      mkdir -p $name
-    else
-      echo $size > $name
-    fi
+    [ "$size" == "dir" ] && mkdir -p $name || echo $size > $name
   fi
 done < <(cat $input)
 
+# Aggregate sizes, deepest dirs first
 while read dir; do
   cd $playground; cd $dir
-  sum=$(find . -type f -maxdepth 1 | xargs cat | awk '{s+=$1} END {print s}')
-  subsum=$(find . -type f -name sum.txt -depth 2 | xargs cat | awk '{s+=$1} END {print s}')
-  total=$((${sum:-0}+${subsum:-0}))
-  echo $total > sum.txt
+  {
+    find . -type f -maxdepth 1;
+    find . -type f -name sum.txt -depth 2
+  } | xargs cat | awk '{s+=$1} END {print s}' > sum.txt
 done < <(cd $playground && find . -type d | awk -F "/" '{print NF-1, $0}' | sort -nr | cut -d' ' -f2)
 
+cd $playground
 if [ $part -eq 1 ]; then
-  cd $playground && find . -type f -name sum.txt | xargs cat | awk '{if($1<=100000)print $1}' | awk '{s+=$1} END {print s}'
+  find . -type f -name sum.txt | xargs cat | awk '{if($1<=100000)s+=$1} END {print s}'
 else
-  inuse=$(cat $playground/sum.txt)
-  remaining=$((70000000-inuse))
-  target=$((30000000-remaining))
-  cd $playground && find . -type f -name sum.txt | xargs cat | awk '{if($1>'"$target"')print $1}' | sort -n | head -n 1
+  target=$((30000000-(70000000-$(cat $playground/sum.txt))))
+  find . -type f -name sum.txt | xargs cat | awk '{if($1>'"$target"')print $1}' | sort -n | head -n 1
 fi
-
 cd $old_cwd && rm -rf $playground
