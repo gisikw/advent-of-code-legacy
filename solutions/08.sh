386bf99f2f402d6b895aa8306da2aedf4eac59bc
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-08 01:44:20 -0600

    Cleanup day 8

--- solutions/08.sh
+++ solutions/08.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
 input=$1
 part=${2:-1}
+
+height=$(wc -l $input | awk '{print $1}')
+width=$(($(cat $input | head -n 1 | wc -c | awk '{print $1}')-1))
+map=$(cat $input | tr -d '\n')
+imap=$(cat $input | sed 's/\(.\)/\1,/g; s/,$//' | /usr/bin/rs -T -c, -C, | sed 's/,//g' | tr -d "\n")
 
 mapat() {
   echo ${map:((($2*$width)+$1)):1}
@@ -10,10 +14,15 @@ mapat() {
 isvisible() {
   x=$1; y=$2
   [ $x == 0 ] || [ $y == 0 ] || [ $x == $((width-1)) ] || [ $y == $((height-1)) ] && return 0;
-  row=${map:((($y*$width))):$width}
+
+  row=${map:(($y*$width)):$width}
+  col=${imap:(($x*$height)):$height}
   value=${row:x:1}
-  suffix=${row:$((x+1))}
+
   prefix=${row:0:$x}
+  suffix=${row:$((x+1))}
+  cprefix=${col:0:$y}
+  csuffix=${col:$((y+1))}
 
   # Visible from the left
   [ -z "$(echo $prefix | fold -w 1 | awk '{if($1>='"$value"')print $1}')" ] && return 0;
@@ -21,19 +30,11 @@ isvisible() {
   # Visible from the right 
   [ -z "$(echo $suffix | fold -w 1 | awk '{if($1>='"$value"')print $1}')" ] && return 0;
 
-  # Visible from below
-  obstructed=0
-  for ((y2=y+1; y2<height; y2++)); do
-    [ $(mapat $x $y2) -ge $value ] && obstructed=1 && break
-  done
-  [ $obstructed == 0 ] && return 0
-
   # Visible from above
-  obstructed=0
-  for ((y2=y-1; y2>=0; y2--)); do
-    [ $(mapat $x $y2) -ge $value ] && obstructed=1 && break
-  done
-  [ $obstructed -eq 0 ] && return 0
+  [ -z "$(echo $cprefix | fold -w 1 | awk '{if($1>='"$value"')print $1}')" ] && return 0;
+
+  # Visible from below
+  [ -z "$(echo $csuffix | fold -w 1 | awk '{if($1>='"$value"')print $1}')" ] && return 0;
 
   return 1;
 }
@@ -69,9 +70,6 @@ score() {
   echo $view
 }
 
-height=$(wc -l $input | awk '{print $1}')
-width=$(($(cat $input | head -n 1 | wc -c | awk '{print $1}')-1))
-map=$(cat $input | tr -d '\n')
 if [ $part -eq 1 ]; then
   visible=0
   for ((x=0; x<width; x++)); do
