263718eea89ed9383174123b40089d5cbf464808
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-08 10:04:06 -0600

    Optimizations

--- solutions/08.sh
+++ solutions/08.sh
@@ -12,50 +12,47 @@ mapat() {
 }
 
 if [ $part -eq 1 ]; then
-  visible="0,0
-$((width-1)),$((height-1))
-0,$((height-1))
-$((width-1)),0"
+  visible=("0,0" "$((width-1)),$((height-1))" "0,$((height-1))" "$((width-1)),0")
 
   for ((x=1; x<width-1; x++)); do
-    visible+=$'\n'"$x,0"
-    visible+=$'\n'"$x,$((height-1))"
+    visible+=("$x,0")
+    visible+=("$x,$((height-1))")
 
     # March in from top
     max=$(mapat $x 0)
     for ((y=1; y<height-1; y++)); do
       here=$(mapat $x $y)
-      [ $here -gt $max ] && visible+=$'\n'"$x,$y" && max=$here
+      [ $here -gt $max ] && visible+=("$x,$y") && max=$here
     done
 
     # March in from bottom
     max=$(mapat $x $((height-1)))
     for ((y=height-2; y>=0; y--)); do
       here=$(mapat $x $y)
-      [ $here -gt $max ] && visible+=$'\n'"$x,$y" && max=$here
+      [ $here -gt $max ] && visible+=("$x,$y") && max=$here
     done
   done
 
   for ((y=1; y<height-1; y++)); do
-    visible+=$'\n'"0,$y"
-    visible+=$'\n'"$((width-1)),$y"
+    visible+=("0,$y")
+    visible+=("$((width-1)),$y")
 
     # March in from left
     max=$(mapat 0 $y)
     for ((x=1; x<width-1; x++)); do
       here=$(mapat $x $y)
-      [ $here -gt $max ] && visible+=$'\n'"$x,$y" && max=$here
+      [ $here -gt $max ] && visible+=("$x,$y") && max=$here
     done
 
     # March in from right
     max=$(mapat $((width-1)) $y)
     for ((x=width-2; x>=0; x--)); do
       here=$(mapat $x $y)
-      [ $here -gt $max ] && visible+=$'\n'"$x,$y" && max=$here
+      [ $here -gt $max ] && visible+=("$x,$y") && max=$here
     done
   done
 
-  echo "$visible" | sort | uniq | wc -l | awk '{print $1}'
+  IFS=$'\n' sort<<<"${visible[*]}" | uniq | wc -l | awk '{print $1}'
 else
   for ((x=0; x<width; x++)); do
     for ((y=0; y<height; y++)); do
