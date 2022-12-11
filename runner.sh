ad78bea231cbce7ff7bf49033fda125bd1190983
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-11 01:37:44 -0600

    Day 11 - remove expr for speed

--- runner.sh
+++ runner.sh
@@ -7,16 +7,38 @@ run_with_time() {
   output=$(echo "$stdout_stderr" | head -n $(($lines-4)))
 }
 
-for day in $(seq -w 1 25); do
-  if [[ -f "./solutions/$day.sh" && -f "./inputs/$day.txt" ]]; then
-    echo "Day $day"
+if [ -z "$1" ]; then
+  for day in $(seq -w 1 25); do
+    if [[ -f "./solutions/$day.sh" && -f "./inputs/$day.txt" ]]; then
+      echo "Day $day"
 
-    run_with_time $day 1
-    echo "  Part 1 ($runtime)"
-    echo "$output" | indent
+      run_with_time $day 1
+      echo "  Part 1 ($runtime)"
+      echo "$output" | indent
 
-    run_with_time $day 2
-    echo "  Part 2 ($runtime)"
-    echo "$output" | indent
-  fi
-done
+      run_with_time $day 2
+      echo "  Part 2 ($runtime)"
+      echo "$output" | indent
+    fi
+  done
+  exit 0
+fi
+
+if [ -z "$2" ]; then
+  echo "Day $1"
+
+  run_with_time $1 1
+  echo "  Part 1 ($runtime)"
+  echo "$output" | indent
+
+  run_with_time $1 2
+  echo "  Part 2 ($runtime)"
+  echo "$output" | indent
+  exit 0
+fi
+
+echo "Day $1"
+
+run_with_time $1 $2
+echo "  Part $2 ($runtime)"
+echo "$output" | indent
--- solutions/11.sh
+++ solutions/11.sh
@@ -2,34 +2,30 @@
 input=$1
 part=${2:-1}
 
-items=(); ops=(); divisors=(); true_targets=(); false_targets=(); inspects=()
+items=(); divisors=(); trues=(); falses=(); inspects=()
+ltokens=(); rtokens=(); combinators=();
 
 while read monkey; do
-  read itemline 
-  read opline
-  read divisorline
-  read trueline
-  read falseline
-  read
+  read itemline; read opline; read divisorline; read trueline; read falseline; read
   items+=("$(echo "${itemline:16}" | sed 's/,//g')")
-  op=$(echo '`expr '"${opline:17}"'`' | sed 's/old/$j/g; s/*/\\*/g')
-  ops+=("$op")
+  read ltoken combinator rtoken <<<"${opline:17}"
+  ltokens+=($ltoken); combinators+=("$combinator"); rtokens+=($rtoken)
   divisors+=(${divisorline:19})
-  true_targets+=(${trueline:25})
-  false_targets+=(${falseline:26})
-done < <(cat $input)
+  trues+=(${trueline:25}); falses+=(${falseline:26})
+done <$input
 
 if [ $part -eq 1 ]; then
   for ((round=0; round < 20; round++)); do
     for ((i=0; i < ${#items[@]}; i++)); do
       for j in ${items[i]}; do
         ((inspects[i]++))
-        j=$(eval "echo" ${ops[i]})
-        j=$((j/3))
+        left=${ltokens[i]}; [ $left == "old" ] && left=$j
+        right=${rtokens[i]}; [ $right == "old" ] && right=$j
+        if [ "${combinators[i]}" == "+" ]; then j=$(((left+right)/3)); else j=$((left*right/3)); fi
         if [ $((j%${divisors[i]})) -eq 0 ]; then
-          items[${true_targets[i]}]+=" $j"
+          items[${trues[i]}]+=" $j"
         else
-          items[${false_targets[i]}]+=" $j"
+          items[${falses[i]}]+=" $j"
         fi
       done
       items[$i]=""
@@ -45,24 +41,24 @@ else
     done
     [ $state -eq 0 ] && break;
     ((lcd+=$largest_divisor))
-    echo $lcd
   done
   for ((round=0; round < 10000; round++)); do
-    echo "$round"
     for ((i=0; i < ${#items[@]}; i++)); do
       for j in ${items[i]}; do
         ((inspects[i]++))
-        j=$(eval "echo" ${ops[i]})
+        left=${ltokens[i]}; [ $left == "old" ] && left=$j
+        right=${rtokens[i]}; [ $right == "old" ] && right=$j
+        if [ "${combinators[i]}" == "+" ]; then j=$((left+right)); else j=$((left*right)); fi
         j=$((j%lcd))
         if [ $((j%${divisors[i]})) -eq 0 ]; then
-          items[${true_targets[i]}]+=" $j"
+          items[${trues[i]}]+=" $j"
         else
-          items[${false_targets[i]}]+=" $j"
+          items[${falses[i]}]+=" $j"
         fi
-        items[$target]+=" $j"
       done
       items[$i]=""
     done
   done
 fi
+
 echo "${inspects[@]}" | tr " " "\n" | sort -nr | head -n 2 | tr "\n" " " | awk '{print $1*$2}'
