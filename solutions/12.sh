023080ecd6fe7c4fa17c05306166b85a1323826f
Kevin Gisi <kevin.gisi@calendly.com>
2022-12-12 01:55:38 -0600

    Day 12 performance refactor

--- solutions/12.sh
+++ solutions/12.sh
@@ -3,7 +3,7 @@ input=$1
 part=${2:-1}
 
 elevation="abcdefghijklmnopqrstuvwxyz"
-map=(); distances=()
+map=(); distances=(); queue=();
 
 if [ $part -eq 1 ]; then
   height=0; i=0;
@@ -11,6 +11,8 @@ if [ $part -eq 1 ]; then
     [ -z "$width" ] && width=$(echo "$row" | wc -c | awk '{ print $1 }'); 
     ((height++))
 
+    start_substring=${row/*S}; start_idx=$((${#row}-${#start_substring}))
+    [ $start_idx -ne 0 ] && queue+=($((start_idx-1)) $i)
     end_substring=${row/*E}; end_idx=$((${#row}-${#end_substring}))
     [ $end_idx -ne 0 ] && end_x=$((end_idx-1)) && end_y=$i
 
@@ -27,54 +29,61 @@ if [ $part -eq 1 ]; then
   done <$input
   ((width--))
 
-  for ((depth = 0; ;depth++)); do
-    for ((x=0; x<$width; x++)); do
-      for ((y=0; y<$height; y++)); do
-        if [ "${distances[((y*width+x))]}" == "$depth" ]; then
-          if [ $x -eq $end_x ] && [ $y -eq $end_y ]; then break 3; fi
-          val=${map[((y*width+x))]}; next_depth=$((depth+1)); compval=$((val+1))
+  while true; do
+    x=${queue[0]}; queue=("${queue[@]:1}")
+    y=${queue[0]}; queue=("${queue[@]:1}")
+    depth=$((${distances[((y*width+x))]}+1))
 
-          # Up
-          if [ $y -gt 0 ] && [ ${map[(((y-1)*width+x))]} -le $compval ]; then
-            d=${distances[(((y-1)*width+x))]}
-            if [ "$d" == "x" ] || [ $d -gt $next_depth ]; then
-              distances[$(((y-1)*width+x))]=$next_depth
-            fi
-          fi
+    if [ $x -eq $end_x ] && [ $y -eq $end_y ]; then
+      echo $((depth-1)) && exit 0;
+    fi
+    val=${map[((y*width+x))]}; compval=$((val+1))
 
-          # Down
-          if [ $y -lt $((height-1)) ] && [ ${map[(((y+1)*width+x))]} -le $compval ]; then
-            d=${distances[(((y+1)*width+x))]}
-            if [ "$d" == "x" ] || [ $d -gt $next_depth ]; then
-              distances[$(((y+1)*width+x))]=$next_depth
-            fi
-          fi
+    # Up
+    if [ $y -gt 0 ] && [ ${map[(((y-1)*width+x))]} -le $compval ]; then
+      d=${distances[(((y-1)*width+x))]}
+      if [ "$d" == "x" ] || [ $d -gt $depth ]; then
+        distances[$(((y-1)*width+x))]=$depth
+        queue+=("$x" "$((y-1))")
+      fi
+    fi
 
-          # Left
-          if [ $x -gt 0 ] && [ ${map[((y*width+x-1))]} -le $compval ]; then
-            d=${distances[((y*width+x-1))]}
-            if [ "$d" == "x" ] || [ $d -gt $next_depth ]; then
-              distances[$((y*width+x-1))]=$next_depth
-            fi
-          fi
+    # Down
+    if [ $y -lt $((height-1)) ] && [ ${map[(((y+1)*width+x))]} -le $compval ]; then
+      d=${distances[(((y+1)*width+x))]}
+      if [ "$d" == "x" ] || [ $d -gt $depth ]; then
+        distances[$(((y+1)*width+x))]=$depth
+        queue+=("$x" "$((y+1))")
+      fi
+    fi
 
-          # Right
-          if [ $x -lt $((width-1)) ] && [ ${map[((y*width+x+1))]} -le $compval ]; then
-            d=${distances[((y*width+x+1))]}
-            if [ "$d" == "x" ] || [ $d -gt $next_depth ]; then
-              distances[$((y*width+x+1))]=$next_depth
-            fi
-          fi
-        fi
-      done
-    done
+    # Left
+    if [ $x -gt 0 ] && [ ${map[((y*width+x-1))]} -le $compval ]; then
+      d=${distances[((y*width+x-1))]}
+      if [ "$d" == "x" ] || [ $d -gt $depth ]; then
+        distances[$((y*width+x-1))]=$depth
+        queue+=("$((x-1))" "$y")
+      fi
+    fi
+
+    # Right
+    if [ $x -lt $((width-1)) ] && [ ${map[((y*width+x+1))]} -le $compval ]; then
+      d=${distances[((y*width+x+1))]}
+      if [ "$d" == "x" ] || [ $d -gt $depth ]; then
+        distances[$((y*width+x+1))]=$depth
+        queue+=("$((x+1))" "$y")
+      fi
+    fi
+
   done
-  echo ${distances[$((end_y*width+end_x))]}
 else
-  height=0
+  height=0; i=0;
   while read row; do
     [ -z "$width" ] && width=$(echo "$row" | wc -c | awk '{ print $1 }'); 
     ((height++))
+
+    end_substring=${row/*E}; end_idx=$((${#row}-${#end_substring}))
+    [ $end_idx -ne 0 ] && queue+=($((end_idx-1)) $i)
 
     # Convert letters to numbers
     for letter in $(echo $row | sed 's/S/a/;s/E/z/;s/\(.\)/\1 /g'); do
@@ -83,53 +92,56 @@ else
       map+=($idx)
     done
 
-    # Convert S -> 0, ^S -> x
+    # Convert E -> 0, ^E -> x
     distances+=($(echo $row | sed 's/E/0/;s/[^0]/x/g;s/\(.\)/\1 /g'))
+    ((i++))
   done <$input
   ((width--))
   
-  for ((depth = 0; ;depth++)); do
-    for ((x=0; x<$width; x++)); do
-      for ((y=0; y<$height; y++)); do
-        if [ "${distances[((y*width+x))]}" == "$depth" ]; then
-          val=${map[((y*width+x))]}; 
-          if [ $val -eq 1 ]; then break 3; fi
-          next_depth=$((depth+1)); compval=$((val-1))
+  while true; do
+    x=${queue[0]}; queue=("${queue[@]:1}")
+    y=${queue[0]}; queue=("${queue[@]:1}")
+    depth=$((${distances[((y*width+x))]}+1))
 
-          # Up
-          if [ $y -gt 0 ] && [ ${map[(((y-1)*width+x))]} -ge $compval ]; then
-            d=${distances[(((y-1)*width+x))]}
-            if [ "$d" == "x" ] || [ $d -gt $next_depth ]; then
-              distances[$(((y-1)*width+x))]=$next_depth
-            fi
-          fi
+    val=${map[((y*width+x))]}; compval=$((val-1))
+    if [ $val -eq 1 ]; then
+      echo $((depth-1)) && exit 0;
+    fi
 
-          # Down
-          if [ $y -lt $((height-1)) ] && [ ${map[(((y+1)*width+x))]} -ge $compval ]; then
-            d=${distances[(((y+1)*width+x))]}
-            if [ "$d" == "x" ] || [ $d -gt $next_depth ]; then
-              distances[$(((y+1)*width+x))]=$next_depth
-            fi
-          fi
+    # Up
+    if [ $y -gt 0 ] && [ ${map[(((y-1)*width+x))]} -ge $compval ]; then
+      d=${distances[(((y-1)*width+x))]}
+      if [ "$d" == "x" ] || [ $d -gt $depth ]; then
+        distances[$(((y-1)*width+x))]=$depth
+        queue+=("$x" "$((y-1))")
+      fi
+    fi
 
-          # Left
-          if [ $x -gt 0 ] && [ ${map[((y*width+x-1))]} -ge $compval ]; then
-            d=${distances[((y*width+x-1))]}
-            if [ "$d" == "x" ] || [ $d -gt $next_depth ]; then
-              distances[$((y*width+x-1))]=$next_depth
-            fi
-          fi
+    # Down
+    if [ $y -lt $((height-1)) ] && [ ${map[(((y+1)*width+x))]} -ge $compval ]; then
+      d=${distances[(((y+1)*width+x))]}
+      if [ "$d" == "x" ] || [ $d -gt $depth ]; then
+        distances[$(((y+1)*width+x))]=$depth
+        queue+=("$x" "$((y+1))")
+      fi
+    fi
 
-          # Right
-          if [ $x -lt $((width-1)) ] && [ ${map[((y*width+x+1))]} -ge $compval ]; then
-            d=${distances[((y*width+x+1))]}
-            if [ "$d" == "x" ] || [ $d -gt $next_depth ]; then
-              distances[$((y*width+x+1))]=$next_depth
-            fi
-          fi
-        fi
-      done
-    done
+    # Left
+    if [ $x -gt 0 ] && [ ${map[((y*width+x-1))]} -ge $compval ]; then
+      d=${distances[((y*width+x-1))]}
+      if [ "$d" == "x" ] || [ $d -gt $depth ]; then
+        distances[$((y*width+x-1))]=$depth
+        queue+=("$((x-1))" "$y")
+      fi
+    fi
+
+    # Right
+    if [ $x -lt $((width-1)) ] && [ ${map[((y*width+x+1))]} -ge $compval ]; then
+      d=${distances[((y*width+x+1))]}
+      if [ "$d" == "x" ] || [ $d -gt $depth ]; then
+        distances[$((y*width+x+1))]=$depth
+        queue+=("$((x+1))" "$y")
+      fi
+    fi
   done
-  echo $depth
 fi
